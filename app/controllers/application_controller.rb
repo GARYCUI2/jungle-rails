0fe960d176f70ab9a657e82f3517248cd3ad1554
CUI <garycui222@gmail.com>
2022-02-03 21:21:02 -0500

    add admin function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,12 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+
+  def admin_access
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] &&
+      password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
 end
